fced2fbef8b71701c9a4140e4b16b2676ef9155f
rhadek <novotnyr.05@spst.eu>
2022-02-09 08:38:37 +0100

    Součet

--- p01/Form1.cs
+++ p01/Form1.cs
@@ -16,5 +16,12 @@ namespace p01
         {
             InitializeComponent();
         }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int c1 = Convert.ToInt32(textBox1.Text);
+            int c2 = Convert.ToInt32(textBox2.Text);
+            label1.Text = (c1 + c2).ToString();
+        }
     }
 }
